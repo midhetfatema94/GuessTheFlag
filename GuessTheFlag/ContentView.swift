254519c6f37b1855c419a749989063d9e3a3be59
Midhet Sulemani <midhetfatema94@gmail.com>
2020-12-31 15:38:18 +0300

    added accessibility support

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -12,11 +12,24 @@ struct ContentView: View {
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
-    @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score = 0
     @State private var currentFlagName = ""
     @State private var rotationAngle: Double = 0
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
     
     var body: some View {
         ZStack {
@@ -44,12 +57,12 @@ struct ContentView: View {
                             score -= 1
                         }
                         currentFlagName = countries[number]
-//                        showingScore = true
                         self.askQuestion()
                     }, label: {
                         //There are four built-in shapes in Swift: rectangle, rounded rectangle, circle, and capsule
                         Image(self.countries[number])
                             .flagStyle()
+                            .accessibility(label: Text(self.labels[self.countries[number]] ?? "Unknown Flag"))
                     })
                     .rotation3DEffect(Angle(degrees: rotationAngle), axis: (x: 0.0, y: 1.0, z: 0.0))
                 }
@@ -59,11 +72,6 @@ struct ContentView: View {
                     .fontWeight(.bold)
                 Spacer()
             }
-//            .alert(isPresented: $showingScore) {
-//                Alert(title: Text(scoreTitle), message: Text("That is the flag of \(currentFlagName)"), dismissButton: .default(Text("Continue")) {
-//                    self.askQuestion()
-//                })
-//            }
         }
     }
     
---
+++ README.md
@@ -0,0 +1,9 @@
+# GuessTheFlag
+
+A game application to guess the flag from the pictures of flags that are provided.
+
+### Features are:
+* Stacks, Views and Gradients
+* Buttons, Images and Alerts
+* Views and Modifiers
+* Animations and Accessibilty Modifiers
